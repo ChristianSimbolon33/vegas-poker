f7316d5f428b943ae41e80ad41d9e414937eb15e
Christian Simbolon <christiansimbolon33@gmail.com>
2025-05-03 12:01:47 -0700

    Update pokerbot.py

--- pokerbot.py
+++ pokerbot.py
@@ -3,15 +3,15 @@ import deck
 import card
 import itertools
 
-# def generate_combination(a:list, n:int, prevarray:list=[]):
-#     if(len(prevarray) == n):
-#         return [prevarray]
-#     combs = []
-#     for i, val in enumerate(a):
-#         prevarrayextended = prevarray.copy()
-#         prevarrayextended.append(val)
-#         combs += generate_combination(a[i+1:], n, prevarrayextended)
-#     return combs
+def generate_combination(a:list, n:int, prevarray:list=[]):
+    if(len(prevarray) == n):
+        return [prevarray]
+    combs = []
+    for i, val in enumerate(a):
+        prevarrayextended = prevarray.copy()
+        prevarrayextended.append(val)
+        combs += generate_combination(a[i+1:], n, prevarrayextended)
+    return combs
 
 
 class pokerBot():
@@ -34,22 +34,38 @@ class pokerBot():
         for card in self.__hand:
             currentDeck.remove(card)
 
-        avgrewards = []
-        seenhands = []
+        avgRewards = []
+        seenHands = []
+
 
         for move in self.moves:
             hand = self.__hand.copy()
+            numOfReplacements = 0
+
             for i in range(len(move)):
                 if(move[i] == "0"):
-                    for card in currentDeck:
-                        hand[i] = card
+                    numOfReplacements +=1
+            
+            totRewards = 0
+            combinations = generate_combination(currentDeck, numOfReplacements)
+            for comb in combinations:
+                j = 0
+                for i, val in enumerate(move):
+                    if(val == "0"):
+                        hand[i] = comb[j]
+                        j += 1
+                totRewards += self.__pok.bestHand(hand)
+            avgRewards.append(totRewards/len(combinations))
 
-        
-        
-        
-        
-
-
+        index = 0
+        max = 0
+        for i in range(len(avgRewards)):
+            print((self.moves[i], avgRewards[i]))
+        for i, val in enumerate(avgRewards):
+            if val > max:
+                max = val
+                index = i
+        return self.moves[index]
 
 
 if __name__ == "__main__":
@@ -61,10 +77,12 @@ if __name__ == "__main__":
         hand.append(currentDeck.deal())
         
     bot.setHand(hand)
-    bot.move(currentDeck)
+    for card in bot.getHand():
+        print(card)
+    print(bot.move(currentDeck))
 
-    for comb in itertools.combinations([1,2,3,4],3):
-        print(comb)
+    # for comb in itertools.combinations([1,2,3,4],3):
+    #     print(comb)
     #print(generate_combination())
 
 
